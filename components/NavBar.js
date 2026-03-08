10a1a0998ae22113966df15ed49fc04cc8041874
elduder87-eng <elduder87@gmail.com>
2026-03-07 22:20:50 -0600

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,20 @@
-import Link from "next/link"
+"use client";
+
+import Link from "next/link";
 
 export default function NavBar(){
 
 return(
 
-<nav>
+<div className="navbar">
 
-<div style={{display:"flex",alignItems:"center"}}>
+<div className="nav-logo">
 
-<span style={{fontSize:"20px",marginRight:"8px"}}>🌱</span>
+<span style={{fontSize:"22px"}}>🌱</span>
 
 <div>
 
-<div style={{fontWeight:"600"}}>
-Restore
-</div>
+<div style={{fontWeight:600}}>Restore</div>
 
 <div style={{fontSize:"12px",opacity:.6}}>
 Where understanding grows
@@ -26,13 +26,15 @@ Where understanding grows
 
 <div className="nav-links">
 
-<Link href="/dashboard">Dashboard</Link>
+<Link href="/">Dashboard</Link>
+
 <Link href="/chat">Chat</Link>
+
 <Link href="/profile">Profile</Link>
 
 </div>
 
-</nav>
+</div>
 
 )
 
