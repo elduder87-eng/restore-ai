b3f4b0f2df52344221923a6902b22f7f1ad3f3c3
elduder87-eng <elduder87@gmail.com>
2026-03-07 19:23:28 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,18 @@
 
 import "./globals.css"
 import NavBar from "../components/NavBar"
+import { useEffect, useState } from "react"
+import RestoreLogo from "../components/RestoreLogo"
 
 export default function RootLayout({ children }) {
+
+const [loading,setLoading] = useState(true)
+
+useEffect(()=>{
+setTimeout(()=>{
+setLoading(false)
+},2500)
+},[])
 
 return (
 
@@ -11,14 +21,36 @@ return (
 
 <body>
 
-<NavBar/>
+{loading ? (
 
+<div style={{
+display:"flex",
+flexDirection:"column",
+justifyContent:"center",
+alignItems:"center",
+height:"100vh",
+background:"#f4f7fb"
+}}>
+
+<RestoreLogo/>
+
+<h1 style={{marginTop:"20px"}}>Restore</h1>
+<p>Where Understanding Grows</p>
+
+</div>
+
+) : (
+
+<>
+<NavBar/>
 {children}
+</>
+
+)}
 
 </body>
 
 </html>
 
 )
-
 }
