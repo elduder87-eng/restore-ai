f5399bb2bd7e0130504914720a73bc3a73a31f5e
elduder87-eng <elduder87@gmail.com>
2026-03-08 07:23:51 -0500

    Update LoadingScreen.js

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -8,15 +8,18 @@ return (
 
 <div className="intro-particles"></div>
 
-{/* Falling seeds */}
+{/* Falling Seeds */}
 
 <div className="seed seed1">🌱</div>
 <div className="seed seed2">🌱</div>
 <div className="seed seed3">🌱</div>
 <div className="seed seed4">🌱</div>
 <div className="seed seed5">🌱</div>
+<div className="seed seed6">🌱</div>
+<div className="seed seed7">🌱</div>
+<div className="seed seed8">🌱</div>
 
-{/* Main seed */}
+{/* Main Seed */}
 
 <div className="seed-drop">🌱</div>
 
