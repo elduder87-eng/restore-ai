c87f1ff3ae7c0139d7d9b11647544d9a21b61191
elduder87-eng <elduder87@gmail.com>
2026-03-08 07:55:46 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,31 +1,9 @@
-"use client"
-
-import { useState,useEffect } from "react"
-import LoadingScreen from "../components/LoadingScreen"
-import Dashboard from "./dashboard/page"
-
 export default function Home(){
 
-const [loading,setLoading] = useState(true)
-
-useEffect(()=>{
-
-const timer = setTimeout(()=>{
-
-setLoading(false)
-
-},3500)
-
-return ()=>clearTimeout(timer)
-
-},[])
-
-if(loading){
-
-return <LoadingScreen/>
-
+if(typeof window !== "undefined"){
+window.location.href="/intro.html"
 }
 
-return <Dashboard/>
+return null
 
 }
