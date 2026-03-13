cf6bb9e26ffaf4de0aff36a547456a7462002f9e
elduder87-eng <elduder87@gmail.com>
2026-03-13 12:17:07 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import "./globals.css"
+
 export const metadata = {
   title: "Restore",
-  description: "Where understanding grows"
+  description: "Where understanding grows",
 }
-
-import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
