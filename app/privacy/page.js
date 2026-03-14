b9bf572fddda440c7930d0a448e7042f994627b6
elduder87-eng <elduder87@gmail.com>
2026-03-14 08:51:17 -0500

    Create page.js

---
+++ app/privacy/page.js
@@ -0,0 +1,15 @@
+export default function Privacy() {
+  return (
+    <iframe
+      src="/privacy.html"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: 'none',
+      }}
+    />
+  )
+}
