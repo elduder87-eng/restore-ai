e7b4fc156bded46d0808458b7668114e9e2bab1d
elduder87-eng <elduder87@gmail.com>
2026-04-07 21:07:03 -0500

    Update ClerkProvider URLs to include .html

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider afterSignInUrl="/universe" afterSignUpUrl="/universe">
+    <ClerkProvider afterSignInUrl="/universe.html" afterSignUpUrl="/universe.html">
       <html lang="en">
         <body>{children}</body>
       </html>
